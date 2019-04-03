ea40af6ae0c267602c577d1f8ce8b73bff67fd80
Dan Steinman <dansteinman@gmail.com>
2019-04-03 19:13:18 -0400

    add continuous VAD segmenting test code

---
+++ test/continuous.js
@@ -0,0 +1,14 @@
+let Listen = require('../lib/listen');
+
+let listen = new Listen();
+
+listen.on('recognize', function(text) {
+	console.log('Recognized Text:', text);
+});
+
+listen.startContinuous();
+
+// setTimeout(function() {
+// 	listen.stopContinuous();
+// 	process.exit();
+// },20000);

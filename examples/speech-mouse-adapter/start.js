9a959411ec9f3e74fc230be6a8e932b69085050e
Dan Steinman <dansteinman@gmail.com>
2019-12-16 08:14:39 -0500

    add momentary recording in speech-mouse adapter

--- examples/speech-mouse-adapter/start.js
+++ examples/speech-mouse-adapter/start.js
@@ -6,6 +6,12 @@ const jaxcore = new Jaxcore();
 jaxcore.addPlugin(SpeechPlugin);
 jaxcore.addPlugin(require('jaxcore/plugins/keyboard'));
 jaxcore.addPlugin(require('jaxcore/plugins/mouse'));
+jaxcore.addPlugin(require('jaxcore/plugins/scroll'));
+
+const readline = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
 
 class SpeechMouseAdapter extends Adapter {
 	static getDefaultState() {
@@ -16,33 +22,53 @@ class SpeechMouseAdapter extends Adapter {
 	constructor(store, config, theme, devices, services) {
 		super(store, config, theme, devices, services);
 		const {speech} = devices;
-		const {mouse, keyboard} = services;
-		
-		console.log('mouse', mouse);
-		console.log('keyboard', keyboard);
-		
-		// spin.rainbow(2);
-		// spin.lightsOff();
+		const {mouse, keyboard, scroll} = services;
 		
 		this.addEvents(speech, {
 			recognize: function (text, stats) {
-				console.log('speech recognize', text);
+				this.log('speech recognize', text, stats);
+			},
+			start: function() {
+				this.log('on start');
+			},
+			stop: function(stats) {
+				this.log('on stop', stats);
 			}
 		});
 		
-		console.log('in 7.....');
-		setTimeout(function() {
-			speech.startContinuous();
-		}, 7000);
+		// console.log('in 7.....');
+		// setTimeout(function() {
+		// 	speech.startContinuous();
+		// }, 2000);
+		
+		process.stdin.resume();
+		
+		this.startConsoleLoop();
+		
+	}
+	
+	startConsoleLoop() {
+		const {speech} = this.devices;
+		
+		readline.question('\nPress ENTER to start recording.\n', () => {
+			
+			readline.question('Press ENTER to stop.\n', (name) => {
+				speech.stop();
+				console.log('Stopped recording.');
+				this.startConsoleLoop();
+			});
+			
+			speech.start();
+		});
 	}
 	
 	static getServicesConfig(adapterConfig) {
 		console.log('getServicesConfig', adapterConfig);
-		console.log('getServicesConfig', adapterConfig);
 		// process.exit();
 		return {
 			keyboard: true,
-			mouse: true
+			mouse: true,
+			scroll: true
 		};
 	}
 }
@@ -50,18 +76,6 @@ class SpeechMouseAdapter extends Adapter {
 jaxcore.addAdapter('speech-mouse', SpeechMouseAdapter);
 
 jaxcore.on('service-connected', function(type, service) {
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	console.log('got service');
-	// process.exit();
 	if (type === 'speech') {
 		jaxcore.launchAdapter(service, 'speech-mouse');
 	}
